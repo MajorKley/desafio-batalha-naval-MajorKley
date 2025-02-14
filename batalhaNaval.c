780da228ab608bea8904428307113f59c196975a
Kleyciane Dutra Mendes <160146713+MajorKley@users.noreply.github.com>
2025-02-14 12:32:40 +0000

    adicionado novas funcionalidades do nivel aventureiro: Tabuleiro 10x10: O tabuleiro será expandido para uma matriz 10x10. Posicionamento de Quatro Navios: O sistema deverá posicionar quatro navios, incluindo dois na diagonal. Exibição Completa do Tabuleiro: O sistema exibirá toda a matriz, onde 0 indica uma posição sem navio e 3 indica uma posição ocupada.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 
-
+#define linhas 10
+#define colunas 10
+#define posicaoNavios 3 
 int main() {
-   //coordenadas x = linhas, y = colunas
-   int x = 4,y = 3;
-   // linhas serão em letras, colunas em números
-   char *tabuleiro [5][5] = {{"a1", "a2", "a3", "a4", "a5"},
-                             {"b1", "b2", "b3", "b4", "b5"},
-                             {"c1", "c2", "c3", "c4", "c5"},
-                             {"d1", "d2", "d3", "d4", "d5"},
-                             {"e1", "e2", "e3", "e4", "e5"}};
+
+//definir valores para matriz
+  int tabuleiro [linhas][colunas];
+  int posicao;
+    //inicilização das posições
+  for(int i = 0; i < linhas; i++){
+    for(int j = 0; j < colunas; j++){
+        tabuleiro[i][j] = 0;   }
+    }
+    
+    //posicionamentos navio
+  tabuleiro [7][5] = posicaoNavios;
+  tabuleiro [9][0] = posicaoNavios;
+  tabuleiro [2][2] = posicaoNavios;//nnavios na diagonal
+  tabuleiro [3][3] = posicaoNavios;//navio na diagonal
+
+  for(int i = 0; i < linhas; i++){
+    for(int j = 0; j < colunas; j++){
+        printf("%d ", tabuleiro[i][j]);}
+  
+    printf("\n");
+  }
+
+  
                              
     //0 será o número padrão para os indíces que não serão mudados
-    printf("A posição do barco horizontal é: %s \n", tabuleiro[x][0]);
-    printf("A posição do barco vertical é: %s \n", tabuleiro[0][y]);
+    // printf("A posição do barco horizontal é: %s \n", tabuleiro[x][0]);
+    // printf("A posição do barco vertical é: %s \n", tabuleiro[0][y]);
 
     
                             
